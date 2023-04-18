6fe154e8d86966c369d4e7e26400ea8fb5e8fe5d
Juanma <pedrozo.juanma@gmail.com>
2023-04-18 19:52:09 -0300

    homepage added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,29 @@
-import { Box, Typography, useTheme } from "@mui/material"
+import { Box, Divider, Typography, useTheme } from "@mui/material"
 
 export default () => {
     const theme = useTheme()
     return (<Box sx={{
-        bgcolor: theme.schemes.light.onPrimaryContainer
+        bgcolor: theme.schemes.light.onPrimaryContainer,
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 3,
+        flexDirection: 'column'
     }}>
-        <Typography variant="h1">This is my shitty home</Typography>
+        <Box sx={{
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 3,
+            flexDirection: 'column'
+        }}>
+            <Typography variant="h1" fontWeight={'bold'} fontSize={45} color={theme.schemes.light.onPrimary}>Juan Martin Pedrozo</Typography>
+            <hr/>
+            <Typography variant="h2" fontWeight={'light'} fontSize={24} color={theme.schemes.light.onPrimary}>Cloud Architect and Backend Developer</Typography>
+        </Box>
     </Box>)
 }
