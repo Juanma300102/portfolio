c37104c7e46ef41865458f37bee23a1fc913ba05
Juanma <pedrozo.juanma@gmail.com>
2023-04-20 16:46:12 -0300

    streamline homepage 1

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,8 @@
 import { Box, Divider, Typography, useTheme } from "@mui/material"
+import PythonIcon from '../../assets/mdi_language-python.svg'
+import TypescriptIcon from '../../assets/mdi_language-typescript.svg'
+import JavascriptIcon from '../../assets/mdi_language-javascript.svg'
+import AwsIcon from '../../assets/mdi_aws.svg'
 
 export default () => {
     const theme = useTheme()
@@ -19,11 +23,18 @@ export default () => {
             alignItems: 'center',
             justifyContent: 'center',
             padding: 3,
-            flexDirection: 'column'
+            flexDirection: 'column',
+            gap: 1,
         }}>
             <Typography variant="h1" fontWeight={'bold'} fontSize={45} color={theme.schemes.light.onPrimary}>Juan Martin Pedrozo</Typography>
-            <hr/>
+            <Divider sx={{width: '100%', bgcolor: theme.schemes.light.onPrimary}}/>
             <Typography variant="h2" fontWeight={'light'} fontSize={24} color={theme.schemes.light.onPrimary}>Cloud Architect and Backend Developer</Typography>
+            <Box sx={{display: 'flex', gap: 2, paddingY: 1, width: '100%'}}>
+                <img src={PythonIcon}/>
+                <img src={TypescriptIcon}/>
+                <img src={JavascriptIcon}/>
+                <img src={AwsIcon}/>
+            </Box>
         </Box>
     </Box>)
 }
