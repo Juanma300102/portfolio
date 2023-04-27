40525bf9b5503439a628b864ea6654e951dc2d50
Juanma <pedrozo.juanma@gmail.com>
2023-04-27 18:03:09 -0300

    particles fixed

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,7 +10,77 @@ import { useCallback } from "react"
 import type { Container, Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
 import Particles from "react-particles"
+import { ISourceOptions, IMove } from 'tsparticles-engine'
 
+const backgroudParticlesConfig: ISourceOptions = {
+    fpsLimit: 120,
+    interactivity: {
+        events: {
+            onClick: {
+                enable: true,
+                mode: "push",
+            },
+            onHover: {
+                enable: true,
+                mode: "repulse",
+            },
+            resize: true,
+        },
+        modes: {
+            push: {
+                quantity: 0,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+        },
+    },
+    particles: {
+        links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: false,
+            opacity: 0.5,
+            width: 1,
+        },
+        collisions: {
+            enable: false,
+        },
+        move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+                default: "out" ,
+            },
+            random: false,
+            speed: 6,
+            straight: false,
+        },
+        number: {
+            density: {
+                enable: false,
+                area: 800,
+            },
+            value: 5,
+        },
+        opacity: {
+            value: 1,
+        },
+        shape: {
+            type: "image",
+            image: {
+              src: "https://drive.google.com/uc?id=1ACoGr8G-C_8H158nVmLeDTgU6NDV-YWD",
+              width: 100,
+              height: 100
+            }
+        },
+        size: {
+            value: 300
+        }
+    },
+    detectRetina: true,
+}
 
 const imgDefaultStyle: object = {
     position: 'absolute',
@@ -67,6 +137,7 @@ export default () => {
                 </Box>
             </Box>
         </Box>
+        <Box sx={{overflow: 'clip', display: 'none'}}>
         <img style={{
             ...imgDefaultStyle,
             width: '420px',
@@ -88,82 +159,12 @@ export default () => {
             top: '522px',
             left: '-51px'
         }} src={Blob3}/>
+        </Box>
         <Particles
             id="tsparticles"
             init={particlesInit}
             loaded={particlesLoaded}
-            options={{
-                background: {
-                    color: {
-                        value: "#0d47a1",
-                    },
-                },
-                fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 4,
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
-                },
-                particles: {
-                    color: {
-                        value: "#ffffff",
-                    },
-                    links: {
-                        color: "#ffffff",
-                        distance: 150,
-                        enable: true,
-                        opacity: 0.5,
-                        width: 1,
-                    },
-                    collisions: {
-                        enable: true,
-                    },
-                    move: {
-                        direction: "none",
-                        enable: true,
-                        outModes: {
-                            default: "bounce",
-                        },
-                        random: false,
-                        speed: 6,
-                        straight: false,
-                    },
-                    number: {
-                        density: {
-                            enable: true,
-                            area: 800,
-                        },
-                        value: 80,
-                    },
-                    opacity: {
-                        value: 0.5,
-                    },
-                    shape: {
-                        type: "circle",
-                    },
-                    size: {
-                        value: { min: 1, max: 5 },
-                    },
-                },
-                detectRetina: true,
-            }}
+            options={backgroudParticlesConfig}
         />
 
     </Box>)
