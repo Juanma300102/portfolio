0e977197c48e5aef8080195f22d1474118f790c1
Juanma <pedrozo.juanma@gmail.com>
2023-05-04 18:32:25 -0300

    fix theme.ts

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,24 +1,20 @@
-import { Components, SxProps, Theme, createTheme } from "@mui/material";
-import a from './theme.json'
+import { createTheme } from "@mui/material";
+import a from "./theme.json";
 
-declare module '@mui/material/styles' {
-    interface ITheme {
-      components?: Components<Theme>,
-      unstable_sx: (props: SxProps<Theme>) => CSSObject,
-      unstable_sxConfig: SxConfig,
-      schemes: {
-        light: any,
-        dark: any
-      } 
-    }
-    // allow configuration using `createTheme`
-    interface ThemeOptions {
-      schemes?: {
-        light?: any,
-        dark?: any
-      }
-    }
+declare module "@mui/material/styles" {
+  interface Theme {
+    schemes: {
+      light: any;
+      dark: any;
+    };
   }
-  
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    schemes?: {
+      light?: any;
+      dark?: any;
+    };
+  }
+}
 
 export const theme = createTheme(a);
