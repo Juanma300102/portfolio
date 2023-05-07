ab612e47eab4dab0ba0cceebdb4937af9eb96288
Juanma <pedrozo.juanma@gmail.com>
2023-05-07 19:47:46 -0300

    update constants

--- src/layouts/mobile/index.tsx
+++ src/layouts/mobile/index.tsx
@@ -11,12 +11,17 @@ import { Nav } from "./components";
 export default () => {
   const particlesInit = useCallback(async (engine: Engine) => {
     await loadFull(engine);
+    console.log(engine);
   }, []);
   const particlesLoaded = useCallback(async () => {}, []);
+
   const theme: any = useTheme();
   return (
     <Box
       sx={{
+        position: "absolute",
+        top: "0px",
+        left: "0px",
         width: "100vw",
         height: "100vh",
         boxSizing: "border-box",
@@ -26,13 +31,25 @@ export default () => {
     >
       <Outlet />
       <Nav />
-      <Particles
-        id="tsparticles"
-        style={{ position: "absolute", zIndex: 1 }}
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={backgroudParticlesConfig}
-      />
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+        }}
+      >
+        <Particles
+          id="tsparticles"
+          width="100vw"
+          height="100vh"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          canvasClassName="particles-canvas"
+          options={backgroudParticlesConfig}
+        />
+      </div>
     </Box>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { routes } from './routes';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from './theme';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routes } from "./routes";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { theme } from "./theme";
+import "./main.css";
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<CssBaseline/>
-		<ThemeProvider theme={theme}>
-			<RouterProvider router={router}/>
-		</ThemeProvider>
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
